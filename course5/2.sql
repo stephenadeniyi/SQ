ebdf9c80b9ce76c7a82e987d8f1baa7e6742659e
stephenadeniyi <adeniyis687@gmail.com>
2025-03-07 19:59:48 -0600

    Update 2.sql

--- course5/2.sql
+++ course5/2.sql
@@ -1,1 +1,17 @@
-dhbdd
+SELECT 
+  station_id,
+  name
+FROM
+  bigquery-public-data.new_york.citibike_stations
+WHERE
+  station_id IN
+
+  (
+    SELECT
+      start_station_id
+    FROM
+      bigquery-public-data.new_york.citibike_trips
+    WHERE
+      usertype =
+      'subscriber'
+  )
